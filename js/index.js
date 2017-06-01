bf3607146c7be0d3acb1d9b61b1ee08b87acc25b
Duane Sibilly <duane@sibilly.com>
2017-06-01 14:22:44 -0400

    Add reduce to index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 import filter from './filter';
 import map from './map';
 import mapSeries from './mapSeries';
+import reduce from './reduce';
 import series from './series';
 
 export {
     filter,
     map,
     mapSeries,
+    reduce,
     series
 };
